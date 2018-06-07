eea48925a2c7f75102d734d8b27651a63feb3977
kazupong <kzpng2@gmail.com>
2018-06-06 17:43:55 -0700

    Initial Commit

---
+++ faide-lens-demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  faide-lens-demo
+//
+//  Created by Kazuyuki Nakatsu on 6/6/18.
+//  Copyright © 2018 Kazuyuki Nakatsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
